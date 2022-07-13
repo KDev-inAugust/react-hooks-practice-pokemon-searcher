ebbabd04bce7b111c51a70eb8183a56c158a5bec
Kane Mathis <admin@kanemathis.com>
2022-07-13 11:36:38 -0400

    cards added and front/back toggle implemented

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React , {useState} from "react";
 import { Card } from "semantic-ui-react";
 
-function PokemonCard() {
+function PokemonCard({cardData}) {
+
+const [cardSide, setCardSide] = useState("front");
+const {id, name, hp, sprites} = cardData;
+
+
+
+function cardClickToggle(){
+  return (
+ cardSide==="front" ? 
+    setCardSide('back')
+    :
+    setCardSide('front')
+  )
+  }
+
   return (
     <Card>
-      <div>
+      <div onClick={cardClickToggle}>
         <div className="image">
-          <img alt="oh no!" />
+          <img src={cardSide==="front" ? sprites.front : sprites.back} alt="oh no!" />
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {hp}
           </span>
         </div>
       </div>
--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,14 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({cardData}) {
   return (
     <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
+      <h1>{cardData.map((index)=>
+      {
+        return <PokemonCard cardData={index}/>
+      }
+      )}</h1>
     </Card.Group>
   );
 }
